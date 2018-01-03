45a7d7d363f9841b25463956f63822f7914744b3
William Fung <wf2@tcl216-9.cs.williams.edu>
2018-01-03 15:48:13 -0500

    end of day 1

--- js_src/main.js
+++ js_src/main.js
@@ -3,7 +3,7 @@ import ROT from 'rot-js';
 import {Game} from './game.js';
 
 window.onload = function() {
-  console.log)"starting WSRL - window loaded");
+  console.log("starting WSRL - window loaded");
   // Check if rot.js can work on this browser
   if (!ROT.isSupported()) {
     alert("The rot.js library isn't supported by your browser.");
@@ -11,3 +11,10 @@ window.onload = function() {
   }
 
   Game.init();
+
+  // Add the containers to our HTML page
+  document.getElementById('ws-main-display').appendChild(Game.getDisplay('main').getContainer());
+
+  Game.render();
+
+};
--- js_src/util.js
+++ js_src/util.js
@@ -1,3 +1,7 @@
 export function utilAlert() {
   document.write("this is a util function<br/>");
 }
+
+export function existentialCrisis() {
+  console.log("please work or else this will be the end of me");
+}
